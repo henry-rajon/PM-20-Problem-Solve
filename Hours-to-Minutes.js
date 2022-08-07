6fa396cc5de45eac667df59c3f1661359632763f
Raj_Dash <rajdash3472@gmail.com>
2022-08-07 10:42:44 +0600

    Hours to Minutes Provlem Added

---
+++ Hours-to-Minutes.js
@@ -0,0 +1,34 @@
+// Problem-4 Hours to Minutes
+
+// function hoursToMinutes(hours) {
+//     const convertHoursToMinutes = hours * 60;
+//     return convertHoursToMinutes;
+// }
+
+// const myValue = 5;
+// const getResult = hoursToMinutes(myValue);
+// console.log(getResult);
+
+
+// Problem-4.1 Minutes to Hours
+
+// function minutesToHours(minutes) {
+//     const convertMinutesToHours = minutes / 60;
+//     return convertMinutesToHours;
+// }
+
+// const myValue = 120;
+// const getResult = minutesToHours(myValue);
+// console.log(getResult);
+
+
+// Problem-4.3 Minutes to Second
+
+// function minutesToSeconds(minutes) {
+//     const convertMinutesToSeconds = minutes * 60;
+//     return convertMinutesToSeconds;
+// }
+
+// const myValue = 60;
+// const getResult = minutesToSeconds(myValue);
+// console.log(getResult);
