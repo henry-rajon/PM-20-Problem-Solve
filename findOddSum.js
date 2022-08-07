ba342aca3de2300332dea710eaa587c5ad158cf9
Raj_Dash <rajdash3472@gmail.com>
2022-08-08 05:20:07 +0600

    Practice Problem 3 solved

---
+++ findOddSum.js
@@ -0,0 +1,16 @@
+// Preactise Problem-3
+
+function getSumOfAllNumber(numbers) {
+    let sum = 1;
+    for (let i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        sum = sum + element;
+        // console.log(index, element, sum);
+    }
+    return sum;
+}
+
+const myNumber = [5, 7, 8, 10, 45, 30];
+const getResultOfSum = getSumOfAllNumber(myNumber);
+console.log(getResultOfSum);
